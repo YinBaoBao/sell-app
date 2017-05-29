7f9826ee70d9e04e76f9012fa611a98a27a5d954
yinbaobao <1420308933@qq.com>
2017-05-29 08:59:22 +0800

    Initial commit

---
+++ src/components/header/header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+  export default {
+    name: '',
+
+    props: {
+    },
+
+    data () {
+    },
+
+    beforeCreate () {
+    },
+
+    created () {
+    },
+
+    beforeMount () {
+    },
+
+    mounted () {
+    },
+
+    beforeUpdate () {
+    },
+
+    updated () {
+    },
+
+    activated () {
+    },
+
+    deactivated () {
+    },
+
+    beforeDestroy () {
+    },
+
+    destroyed () {
+    },
+
+    computed: {},
+
+    methods: {
+    },
+
+    watch: {},
+
+    components: {}
+  }
+</script>
+
+<style lang="">
+@import '';
+</style>
